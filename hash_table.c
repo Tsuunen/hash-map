26bece19c8133c397b22fc5a5e6712af36c0f398
Remi LAFORGUE <laforgue.remi@gmail.com>
2025-03-06 17:45:01 +0100

    Simple implementation of HashMaps in C Auto growing

---
+++ hash_table.c
@@ -0,0 +1,184 @@
+#include "hash_table.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int	hash(char *str, int tableSize)
+{
+	int	sum = 0;
+
+	while (*str != 0)
+	{
+		sum += *str;
+		str++;
+	}
+
+	return (sum % tableSize);
+}
+
+HashTable	*initHashTable(int tableSize)
+{
+	int	i = 0;
+	HashTable	*tab = malloc(sizeof(HashTable));
+	if (tab == NULL)
+		return (NULL);
+
+	tab->size = tableSize;
+	tab->table = malloc(sizeof(Node) * tableSize); 
+	if (tab->table == NULL)
+		return (NULL);
+
+	while (i < tableSize)
+	{
+		tab->table[i] = NULL;
+		i++;
+	}
+
+	return (tab);
+}
+
+void	freeHashTable(HashTable *tab)
+{
+	Node	*curr;
+	Node	*aux;
+	int		i = 0;
+
+	while (i < tab->size)
+	{
+		curr = (tab->table)[i];
+		while (curr != NULL)
+		{
+			aux = curr;
+			curr = curr->next;
+			free(aux);
+		}
+		i++;
+	}
+
+	free(tab->table);
+	free(tab);
+}
+
+void	showHashTable(HashTable *tab)
+{
+	Node	*curr;
+	int		i = 0;
+
+	while (i < tab->size)
+	{
+		printf("%d : \n", i);
+		curr = (tab->table)[i];
+		while (curr != NULL)
+		{
+			printf("%s -> %d\n", curr->key, curr->value);
+			curr = curr->next;
+		}
+		printf("\n");
+		i++;
+	}
+}
+
+void	insert(HashTable **tab, char *key, int value)
+{
+	HashTable	*test = *tab;
+	int 	hashedKey = hash(key, test->size);
+	Node	*new = malloc(sizeof(Node));
+	if (new == NULL)
+		return ;
+
+	if (search(test, key) != NULL)
+	{
+		free(new);
+		return ;
+	}
+
+	strcpy(new->key, key);
+	new->value = value;
+
+	new->next = test->table[hashedKey];
+	test->table[hashedKey] = new;
+
+	if (isQuiteFull(test))
+		enhanceHashTableSize(tab);
+}
+
+Node	*search(HashTable *tab, char *key)
+{
+	int		hashedKey = hash(key, tab->size);
+	Node	*curr = tab->table[hashedKey];
+
+	while (curr != NULL)
+	{
+		if (!strcmp(curr->key, key))
+			return (curr);
+		curr = curr->next;
+	}
+	return (NULL);
+}
+
+void	deleteFromHashTable(HashTable *tab, char *key)
+{
+	int		hashedKey = hash(key, tab->size);
+	Node	*curr = tab->table[hashedKey];
+	Node	*aux = NULL;
+
+	if (curr == NULL)
+		return ;
+	if (!strcmp(curr->key, key))
+	{
+		aux = curr;
+		tab->table[hashedKey] = curr->next;
+		free(aux);
+		return ;
+	}
+	while (curr->next != NULL)
+	{
+		if (!strcmp(curr->next->key, key))
+		{
+			aux = curr->next;
+			curr->next = curr->next->next;
+			free(aux);
+			return ;
+		}
+		curr = curr->next;
+	}
+}
+
+bool	isQuiteFull(HashTable *tab)
+{
+	int	i = 0;
+	int	usedRow = 0.0;
+
+	while (i < tab->size)
+	{
+		if (tab->table[i] != NULL)
+			usedRow++;
+		i++;
+	}
+
+	return ((usedRow / (float)tab->size) >= 0.75);
+}
+
+void    enhanceHashTableSize(HashTable **tab)
+{
+	HashTable	*new = NULL;
+	HashTable	*tmp = *tab;
+	int			i = 0;
+	Node		*curr;
+
+	new = initHashTable(tmp->size * 2);
+
+	while (i < tmp->size)
+	{
+		curr = tmp->table[i];
+		while (curr != NULL)
+		{
+			insert(&new, curr->key, curr->value);
+			curr = curr->next;
+		}
+		i++;
+	}
+
+	*tab = new;
+	freeHashTable(tmp);
+}
---
+++ hash_table.h
@@ -0,0 +1,29 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include <stdbool.h>
+
+typedef struct _Node
+{
+	char			key[50];
+	int				value;
+	struct _Node	*next;
+} Node;
+
+typedef struct
+{
+	Node	**table;
+	int		size;
+} HashTable;
+
+int			hash(char *str, int tableSize);
+HashTable	*initHashTable(int tableSize);
+void		freeHashTable(HashTable *tab);
+void		showHashTable(HashTable *tab);
+void		insert(HashTable * tab, char *key, int value);
+Node		*search(HashTable *tab, char *key);
+void		deleteFromHashTable(HashTable *tab, char *key);
+bool		isQuiteFull(HashTable *tab);
+void		enhanceHashTableSize(HashTable **tab);
+
+#endif
